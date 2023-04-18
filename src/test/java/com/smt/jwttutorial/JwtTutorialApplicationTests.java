4bc5405a77a021f88e58ebca22c08fe31b121a71
so23go <so23go@naver.com>
2023-04-18 14:09:54 +0900

    First Commit

---
+++ src/test/java/com/smt/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smt.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
